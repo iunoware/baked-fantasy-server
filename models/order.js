bbfdff163b756308a514ce0e680805b99d007100
Praveen123-png <praveenkumar814444@gmail.com>
2025-08-19 19:01:35 +0530

    added product

---
+++ models/order.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const orderScheme = new mongoose.Schema({
+  order_id: { type: String, required: true, unique: true },
+  
+});
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import connectDB from "./db.js";
 import Course from "./models/course.js";
+import Product from "./models/products.js";
 import { title } from "process";
 
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(express.json());
 
 connectDB();
 
+// to POST a new course:
 app.post("/add-course", async (req, res) => {
   try {
     const newCourse = await Course.create({
@@ -25,6 +27,23 @@ app.post("/add-course", async (req, res) => {
       duration: "2h",
     });
     res.json(newCourse);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// to POST a new product:
+app.post("/add-products", async (req, res) => {
+  try {
+    const newProduct = await Product.create({
+      name: "product-1",
+      description: "brand new product here",
+      price: 1234,
+      category: "some category",
+      imageUrl: "img.png",
+      inStock: true,
+    });
+    res.json(newProduct);
   } catch (error) {
     res.status(500).send(error.message);
   }
