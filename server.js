586c62eb2f3b480c8ade58e09493f8126f30df35
UmarAbdullah79 <uu0339843@gmail.com>
2025-08-20 16:48:47 +0530

    changes made

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-// common imports
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
+// // common imports
+// import mongoose from "mongoose";
+// import bodyParser from "body-parser";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
 
 // for the models
-import Course from "./models/course.js";
-import Product from "./models/products.js";
-import User from "./models/user.js";
-import Order from "./models/order.js";
+// import Course from "./models/course.js";
+// import Product from "./models/products.js";
+// import User from "./models/user.js";
+// import Order from "./models/order.js";
 
 // for the routes
-import authRoutes from "./routes/authRoutes.js";
-import courseRoutes from "./routes/courseRoutes.js";
-import orderRoutes from "./routes/orderRoutes.js";
-import productRoutes from "./routes/productRoutes.js";
+// // import authRoutes from "./routes/authRoutes.js";
+// import courseRoutes from "./routes/courseRoutes.js";
+// import orderRoutes from "./routes/orderRoutes.js";
+// import productRoutes from "./routes/productRoutes.js";
 
 // other
 import { title } from "process";
@@ -29,27 +29,27 @@ app.use(express.json());
 connectDB();
 
 // for authentication
-app.use("/", authRoutes);
-app.use("/", courseRoutes);
-app.use("/", orderRoutes);
-app.use("/", productRoutes);
+// app.use("/", authRoutes);
+// app.use("/", courseRoutes);
+// app.use("/", orderRoutes);
+// app.use("/", productRoutes);
 
 // to POST a new course:
-app.post("/add-course", async (req, res) => {
-  try {
-    const newCourse = await Course.create({
-      courseId: 101,
-      title: "backend",
-      description: "test run",
-      price: 123,
-      thumbnail: "here goes the thumbnail",
-      duration: "2h",
-    });
-    res.json(newCourse);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
+// app.post("/add-course", async (req, res) => {
+//   try {
+//     const newCourse = await Course.create({
+//       courseId: 101,
+//       title: "backend",
+//       description: "test run",
+//       price: 123,
+//       thumbnail: "here goes the thumbnail",
+//       duration: "2h",
+//     });
+//     res.json(newCourse);
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// });
 
 // to POST a new product:
 // app.post("/add-products", async (req, res) => {
