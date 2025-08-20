850a4e32bdc1b8acc81d26c3f511a5449e1f4a36
Praveen123-png <praveenkumar814444@gmail.com>
2025-08-20 19:15:08 +0530

    created order model

--- models/order.js
+++ models/order.js
@@ -1,7 +1,38 @@
-// import mongoose from "mongoose";
-// import { type } from "os";
+import mongoose from "mongoose";
+import User from "./user";
+import Product from "./products";
 
-// const orderScheme = new mongoose.Schema({
-//   order_id: { type: String, required: true, unique: true },
-  
-// });
+const orderSchema = new mongoose.Schema(
+  {
+    order_id: { type: String, required: true, unique: true },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+      required: true,
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: Product,
+          required: true,
+        },
+        quantity: { type: Number, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    totalPrice: { type: Number, required: true },
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "paid", "failed"],
+      default: "pending",
+    },
+    shippingAddress: { type: String, required: true },
+    billingAddress: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
---
+++ routes/orderRoutes.js
@@ -0,0 +1,6 @@
+import Order from "../models/order.js";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+import router from "./authRoutes.js";
+
+const router = express.Router();
