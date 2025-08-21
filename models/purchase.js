a8bd5a6e564cd7e5d54682b3c949a5df16c26dfd
UmarAbdullah79 <uu0339843@gmail.com>
2025-08-21 19:04:30 +0530

    dummy commit

--- models/purchase.js
+++ models/purchase.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
+let name = "umar";
+let age = 29;
+let king = true;
+
 const purchaseSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   courseId: {
