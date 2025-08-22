a93f0b6cc121572b807c295bbe9e0a4c5a00a682
Praveen123-png <praveenkumar814444@gmail.com>
2025-08-22 19:08:46 +0530

    courseRoute complete and courseVideoRouter partially complete

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -17,32 +17,66 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // to POST a new course:
-router.post(
-  "/course",
-  upload.fields([
-    { name: "video", maxCount: 1 },
-    { name: "image", maxCount: 1 },
-  ]),
-  async (req, res) => {
-    try {
-      const { title, description, price, duration } = req.body;
+router.post("/course", upload.single("image"), async (req, res) => {
+  try {
+    const { title, description, price, duration } = req.body;
 
-      const videoFile = req.files["video"] ? req.files["video"][0] : null;
-      const imageFile = req.files["image"] ? req.files["image"][0] : null;
-
-      const newCourse = await Course.create({
-        title,
-        description,
-        price,
-        thumbnail: imageFile ? `/uploads/${imageFile.filename}` : "No thumbnail provided",
-        duration,
-        videoUrl: videoFile ? `/uploads/${videoFile.filename}` : "No video provided!",
-      });
-      res.json({ msg: "course created successfully", newCourse });
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
+    const newCourse = await Course.create({
+      title,
+      description,
+      price,
+      thumbnail: req.file ? `/uploads/${req.file.filename}` : "No thumbnail provided",
+      duration,
+    });
+    res.json({ msg: "course created successfully", newCourse });
+  } catch (error) {
+    res.status(500).send(error.message);
   }
-);
+});
+
+// to GET all courses:
+router.get("/course", async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.json(courses);
+  } catch (error) {
+    res.status(400).json({ msg: "can't fetch courses", error: error.message });
+  }
+});
+
+// to PATCH courses:
+router.patch("/course/:id", upload.single("image"), async (req, res) => {
+  try {
+    const { title, description, price, duration } = req.body;
+
+    const course = await Course.findById(req.params.id);
+    if (!course) return res.status(400).json({ msg: "course not found" });
+
+    if (title) course.title = title;
+    if (description) course.description = description;
+    if (price) course.price = price;
+    if (req.file) course.thumbnail = `/uploads/${req.file.filename}`;
+    if (duration) course.duration = duration;
+
+    await course.save();
+
+    res.json({ msg: "course updated successfully" });
+  } catch (error) {
+    res.status(400).json({ msg: "can't edit course", error: error.message });
+  }
+});
+
+// DELETING the course:
+router.delete("/course/:id", async (req, res) => {
+  try {
+    const course = await Course.findByIdAndDelete(req.params.id);
+
+    if (!course) return res.status(400).json({ msg: "Course not found" });
+
+    res.json({ msg: "Course DELETED successfully" });
+  } catch (error) {
+    res.status(400).json({ msg: "Failed to delete course", error: error.message });
+  }
+});
 
 export default router;
