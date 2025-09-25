8e0c82cb94ba38d983bf219247089e3d7077928b
Praveen123-png <praveenkumar814444@gmail.com>
2025-09-25 18:44:00 +0530

    added section in course model

--- models/courseVideos.js
+++ models/courseVideos.js
@@ -6,6 +6,7 @@ const courseVideoSchema = new mongoose.Schema({
   title: { type: String, required: true },
   thumbnail: { type: String, required: true },
   videoUrl: { type: String, required: true },
+  section: { type: String, required: true },
   duration: { type: String, required: true },
   category: { type: mongoose.Schema.Types.ObjectId, ref: Course, required: true },
   serialNum: { type: Number, required: true, unique: true },
