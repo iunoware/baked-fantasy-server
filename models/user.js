3f536cc27666dd32babc75f7a2f54a07e5da2dcb
UmarAbdullah79 <uu0339843@gmail.com>
2025-10-08 10:30:43 +0530

    something changed

--- models/user.js
+++ models/user.js
@@ -4,13 +4,6 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String },
-  // confirmPassword: { type: String, required: true },
-  // role: {
-  //   type: String,
-  //   required: true,
-  //   enum: ["user", "admin"],
-  //   default: "user",
-  // },
   purchasedCourses: [
     {
       courseId: { type: mongoose.Schema.Types.ObjectId, ref: "Course" },
