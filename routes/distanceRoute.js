69232d61e5842f5311a2769953123179dc5517ba
UmarAbdullah79 <uu0339843@gmail.com>
2026-03-18 19:01:16 +0530

    distance backend done

---
+++ routes/distanceRoute.js
@@ -0,0 +1,17 @@
+import express from "express";
+import axios from "axios";
+
+const router = express.Router();
+
+// router.post("distance", async(req, res)=>{
+//     const {origin, destination} = req.body;
+
+//     try{
+//         const response = await axios.get("https://maps.googleapis.com/maps/api/distancematrix/json", {
+//             {
+//                 params: {}
+//             }
+//         })
+//     }
+
+// });
