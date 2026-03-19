1b181a2896911d55be42cd2ed9e9293eb848907c
UmarAbdullah79 <uu0339843@gmail.com>
2026-03-19 10:49:36 +0530

    changes maded

--- routes/distanceRoute.js
+++ routes/distanceRoute.js
@@ -3,15 +3,36 @@ import axios from "axios";
 
 const router = express.Router();
 
-// router.post("distance", async(req, res)=>{
-//     const {origin, destination} = req.body;
+router.post("/distance", async (req, res) => {
+  const { origin, destination } = req.body;
 
-//     try{
-//         const response = await axios.get("https://maps.googleapis.com/maps/api/distancematrix/json", {
-//             {
-//                 params: {}
-//             }
-//         })
-//     }
+  try {
+    const response = await axios.get(
+      "https://maps.googleapis.com/maps/api/distancematrix/json",
+      {
+        params: {
+          origins: `${origin.lat},${origin.lng}`,
+          destinations: `${destination.lat},${destination.lng}`,
+          key: process.env.GOOGLE_MAPS_API_KEY,
+        },
+      },
+    );
 
-// });
+    const element = response.data.rows[0].elements[0];
+
+    if (element.status !== "OK") {
+      return res.status(400).json({ error: "Distance not found" });
+    }
+    res.json(element);
+    // res.json({
+    //   distanceText: element.distance.text,
+    //   distanceValue: element.distance.value,
+    //   durationText: element.duration.text,
+    // });
+  } catch (error) {
+    console.error("Distance API error:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+export default router;
